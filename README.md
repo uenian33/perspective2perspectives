8d41756cf5582ded5e711b5dfb9bc72314202e35
uenian33 <44872697+uenian33@users.noreply.github.com>
2022-08-03 17:24:05 +0300

    add initial files

---
+++ README.md
@@ -0,0 +1,28 @@
+# Perspective2Perspective Transform
+
+## Prerequisites
+- Python 3.7
+- [NumPy](http://www.numpy.org/)
+- OpenCV 4.5.4-dev
+
+
+## Usage
+
+You can transfrom one perspective view to another perspective view by running:
+
+```bash
+$ python pers2pers.py 
+```
+
+It will generate:
+- eq2pers.png : The initial perspective projection view
+- pers2pers_eq_mask.png : The initial perspective projection view's on the Equirectangular view
+- pers2pers.png : The transformed perspective view
+- pers2pers_mask.png : The transformed perspective view mask
+
+
+## APIs
+to be added
+
+## Acknowledgements
+* This repo is based on code found in [this PyTorch example repo](https://github.com/timy90022/Perspective-and-Equirectangular)
---
+++ pers2pers.py
@@ -0,0 +1,98 @@
+import os
+import cv2 
+import lib.Equirec2Perspec as E2P
+import lib.Perspec2Equirec as P2E
+import lib.multi_Perspec2Equirec as m_P2E
+import glob
+import argparse
+
+
+
+
+def equir2pers(eq_im,
+               FOV=120,
+               theta=0,
+               phi=0,
+               height=1280,
+               width=1280): # Specify parameters(FOV, theta, phi, height, width)
+
+    #
+    # FOV unit is degree
+    # theta is z-axis angle(right direction is positive, left direction is negative)
+    # phi is y-axis angle(up direction positive, down direction negative)
+    # height and width is output image dimension
+    #
+    
+    #input_img = './panorama/world_map.jpeg'
+    #output_dir = './example/perspective'
+    #if not os.path.exists(output_dir):
+    #    os.mkdir(output_dir)
+    
+    equ = E2P.Equirectangular(eq_im)    # Load equirectangular image
+
+    pers_im = equ.GetPerspective(FOV, theta, phi, height, width)
+    
+    return pers_im
+
+
+def pers2equir(im, FOV=120, 
+               theta=0,
+               phi=0):
+    #
+    # FOV unit is degree
+    # theta is z-axis angle(right direction is positive, left direction is negative)
+    # phi is y-axis angle(up direction positive, down direction negative)
+    # height and width is output image dimension
+    #
+   
+    # this can turn cube to panorama
+    equ = m_P2E.Perspective([im],
+                            [[FOV, theta, phi]])    
+    
+    
+    eq_im, eq_mask = equ.GetEquirec(height,width)  
+    #print(eq_mask)
+    return eq_im, eq_mask
+
+
+
+if __name__ == '__main__':
+    # convert equarectangular to perspective
+    input_img = './panorama/world_map.jpeg'
+    input_img = cv2.imread(input_img, cv2.IMREAD_COLOR)
+    
+    equ = E2P.Equirectangular(input_img)    # Load equirectangular image
+
+    pers_im = equ.GetPerspective(120, 0, 0, 1280, 1280)  # Specify parameters(FOV, theta, phi, height, width)
+    cv2.imwrite('eq2pers.png', pers_im)
+
+
+    # convert one perspective to another
+    input_dir = './example/perspective'
+
+    width = 1920
+    height = 960
+
+    
+    input1 = input_dir + '/perspective_1.png' # 120, 0, 0
+    input2 = input_dir + '/perspective_2.png' # 120, 0, 90
+
+    im1 = cv2.imread(input1, cv2.IMREAD_COLOR)
+    eq, eq_mask = pers2equir(im1)
+
+    new_pers = equir2pers(eq,
+                           FOV=120,
+                           theta=0,
+                           phi=45,)
+
+    new_pers_mask = equir2pers(eq_mask,
+                           FOV=120,
+                           theta=0,
+                           phi=45,)
+
+    cv2.imwrite('pers2eq.png', eq)
+    cv2.imwrite('pers2pers.png', new_pers)
+    cv2.imwrite('pers2pers_mask.png', new_pers_mask)
+    cv2.imwrite('pers2pers_eq_mask.png', eq_mask)
+
+    
